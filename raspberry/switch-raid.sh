04b98836a913afb8a761f0e2eae12b742ba28034
TwizzyDizzy <2012-02-05.github.com@thomasdalichow.de>
2015-08-25 01:13:00 +0200

    Fix hostname and typos, only switch on devices when ACTION is on

--- raspberry/switch-raid.sh
+++ raspberry/switch-raid.sh
@@ -5,7 +5,7 @@
 # is pretty much tailored to my needs so you may use it rather as an
 # inspiration than using it in its current form.
 
-MD_RAID_DEVICE="hostname:0"
+MD_RAID_DEVICE="behring5.chickenkiller.com:0"
 MD_RAID_COMPONENTS="/dev/disk/by-id/usb-WDC_WD50_00AAKX-001CA0_DCAD43109288-0:0 /dev/disk/by-id/usb-WDC_WD50_00AAKX-001CA0_DCAD43109288-0:1"
 
 # what to do with the MD raid, "on" or "off"
@@ -17,11 +17,11 @@ if [[ "$ACTION" != "on" && "$ACTION" != "off" ]]; then
 
 
 
-# switch on physical hard drives, as they are currently sleeping and spun down
-./switch-disk 0 on
-./switch-disk 1 on
-
 if [[ "$ACTION" == "on" ]]; then
+	# switch on physical hard drives, as they are currently sleeping and spun down
+	./switch-disk.sh 0 on
+	./switch-disk.sh 1 on
+
 	# assemble MD raid
 	mdadm --assemble $MD_RAID_DEVICE $MD_RAID_COMPONENTS
 
@@ -39,6 +39,6 @@ else
 	# stopping the MD raid device
 	mdadm --stop /dev/disk/by-id/$MD_RAID_DEVICE
 
-	./switch-disk 0 off
-	./switch-disk 1 off
+	./switch-disk.sh 0 off
+	./switch-disk.sh 1 off
 fi
